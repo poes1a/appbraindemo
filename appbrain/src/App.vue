257cc25041356016f7507bfbe5ddff41e0cd88cb
fcifuentesr <f.cifuentes.rojas@gmail.com>
2025-03-14 22:13:19 -0300

    add: vue app

---
+++ appbrain/src/App.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="flex flex-col h-screen">
+    <!-- Header -->
+    <header class="flex items-center h-14 px-4 border-b border-gray-300 sm:h-16 md:px-6 lg:px-8">
+      <div>
+        <a class="flex items-center gap-2 font-semibold" href="#">
+          <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="100" height="100" />
+        </a>
+      </div>
+      <!-- <nav>
+        <RouterLink to="/venta">Venta</RouterLink>
+        <RouterLink to="/agenda">Agenda</RouterLink>
+      </nav> -->
+    </header>
+    <!-- Fin Header -->
+
+    <!-- Main -->
+    <main class="flex-1 flex items-center justify-center py-6">
+      <div class="text-center">
+        <RouterView />
+      </div>
+    </main>
+    <!-- Fin Main -->
+
+    <!-- Footer -->
+    <footer class="flex items-center h-14 px-4 border-t border-gray-300 sm:h-16 md:px-6 lg:px-8">
+      <p class="flex-1 text-sm text-gray-500 text-center">
+        ©{{ new Date().getFullYear() }} Energy Club.
+      </p>
+    </footer>
+    <!-- Fin Footer -->
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  /* margin: 0 auto 1rem; */
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ appbrain/src/views/VentaView.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1 class="text-4xl font-bold tracking-tighter sm:text-5xl">
+        Venta
+    </h1>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style></style>
